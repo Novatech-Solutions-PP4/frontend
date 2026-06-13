04f5b7fb7b266896a9ec67a2618150b64020e9a2
sanvalos <santiago.avalos@outlook.com>
2026-06-13 12:40:26 +0000

    polish: use dynamic viewport height to show mobile menu

--- src/components/layout/MobileContainer.jsx
+++ src/components/layout/MobileContainer.jsx
@@ -2,7 +2,7 @@
 export default function MobileContainer({ children, className = "", padding = "p-6" }) {
   return (
     <div
-      className={`w-full h-screen bg-white flex flex-col justify-between ${padding} mx-auto relative overflow-hidden ${className}`}
+      className={`w-full h-dvh bg-white flex flex-col justify-between ${padding} mx-auto relative overflow-hidden ${className}`}
     >
       {children}
     </div>
